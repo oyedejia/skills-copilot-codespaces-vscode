b642a1c5ab342f28c4248ef00b468b1e64878ace
oyedejia <151778094+oyedejia@users.noreply.github.com>
2023-11-28 23:53:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,88 +1,82 @@
 //create web server
-//create a web server
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Comments = require('./models/comments');
-var cors = require('cors');
-var jwt = require('jsonwebtoken');
-var User = require('./models/user');
-
-app.use(cors());
-
-//connect to mongoose
-mongoose.connect('mongodb://localhost:27017/comments');
-var db = mongoose.connection;
-
-//create a server
-app.listen(3000);
-
-//get the data from the server
-app.use(bodyParser.json());
-
-//get the data from the server
-app.get('/api/comments', function(req, res){
-	Comments.getComments(function(err, comments){
-		if(err){
-			throw err;
-		}
-		res.json(comments);
-	})
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var mysql = require('mysql');
+//connect to mysql database
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'comments'
 });
 
-//add a comment
-app.post('/api/comments', function(req, res){
-	var comment = req.body;
-	Comments.addComment(comment, function(err, comment){
-		if(err){
-			throw err;
-		}
-		res.json(comment);
-	})
+//connect to mysql database
+connection.connect(function(err) {
+	if(err) {
+		console.log('Error connecting to Db');
+		return;
+	}
+	console.log('Connection established');
 });
 
-//delete a comment
-app.delete('/api/comments/:_id', function(req, res){
-	var id = req.params._id;
-	Comments.removeComment(id, function(err, comment){
-		if(err){
-			throw err;
-		}
-		res.json(comment);
-	})
+//create table in mysql database
+connection.query('CREATE TABLE IF NOT EXISTS comments(id int NOT NULL AUTO_INCREMENT, name VARCHAR(255), comment VARCHAR(255), PRIMARY KEY(id))', function(err) {
+	if(err) throw err;
 });
 
-//update a comment
-app.put('/api/comments/:_id', function(req, res){
-	var id = req.params._id;
-	var comment = req.body;
-	Comments.updateComment(id, comment, {}, function(err, comment){
-		if(err){
-			throw err;
-		}
-		res.json(comment);
-	})
-});
+//create web server
+app.use(express.static('public'));
+app.get('/index.htm', function(req, res) {
+	res.sendFile(__dirname + "/" + "index.htm");
+})
 
-//user registration
-app.post('/api/user', function(req, res){
-	var user = req.body;
-	User.addUser(user, function(err, user){
-		if(err){
-			throw err;
-		}
-		res.json(user);
-	})
-});
+//post comment
+app.post('/process_post', urlencodedParser, function(req, res) {
+	var response = {
+		name: req.body.name,
+		comment: req.body.comment
+	};
+	console.log(response);
+	//insert comment into mysql database
+	connection.query('INSERT INTO comments SET ?', response, function(err, result) {
+		if(err) throw err;
+		console.log(result);
+	});
+	res.end(JSON.stringify(response));
+})
 
-//user login
-app.post('/api/login', function(req, res){
-	var user = req.body;
-	User.getUser(user, function(err, user){
-		if(err){
-			throw err;
-		}
-		res.json(user);
-	})
-});
+//read comments from mysql database
+app.get('/comments', function(req, res) {
+	connection.query('SELECT * FROM comments', function(err, rows, fields) {
+		if(err) throw err;
+		console.log('The solution is: ', rows);
+		res.end(JSON.stringify(rows));
+	});
+})
+
+//delete comment from mysql database
+app.post('/delete_comment', urlencodedParser, function(req, res) {
+	var response = {
+		id: req.body.id
+	};
+	console.log(response);
+	//delete comment from mysql database
+	connection.query('DELETE FROM comments WHERE id = ?', response, function(err, result) {
+		if(err) throw err;
+		console.log(result);
+	});
+	res.end(JSON.stringify(response));
+})
+
+//update comment from mysql database
+app.post('/update_comment', urlencodedParser, function(req, res) {
+	var response = {
+		id: req.body.id,
+		comment: req.body.comment
+	};
+	console.log(response);
+	//update comment from mysql database
+	connection.query
